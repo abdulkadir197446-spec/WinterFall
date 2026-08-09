cd0df6520bc7396144c20a6fd8a48befff207d88
abdulkadir197446-spec <abdulkadir197446@gmail.com>
2026-08-09 12:37:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
+from flask import Flask
+from threading import Thread
 import discord
 from discord.ext import commands
 
+# Render'ın açık kalması için web sunucusu
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot aktif!"
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
+
+keep_alive()
+
+# Bot Kodların
 intents = discord.Intents.default()
 intents.message_content = True
 
@@ -12,4 +31,4 @@ async def on_ready():
     print(f'Giriş yapılan kullanıcı: {bot.user}')
 
 print("Bot başlatılıyor...")
-bot.run('MTUyNzIzMTY3NTEyODQ4Mzk3MQ.G8F0BB.vtjE6Hf-0EkijAmsKGWb0X5SFhR5pAlo6ZtuIc')
+bot.run('MTUyNzIzMTY3NTEyODQ4Mzk3MQ.G8F0BB.vtjE6HF-0EkijAmsKGWb0X5SFhR5pA1o6ZtuIc')
